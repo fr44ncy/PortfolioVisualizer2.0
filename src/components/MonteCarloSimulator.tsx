e93d6839664dbe447a2c4924859670c0d70fbf63
fr44ncy <ifrancy.kg@gmail.com>
2025-11-02 17:03:00 +0100

    Updated MonteCarloSimulator.tsx

--- src/components/MonteCarloSimulator.tsx
+++ src/components/MonteCarloSimulator.tsx
@@ -345,7 +345,6 @@ export default function MonteCarloSimulator({
                     
                   </AreaChart>
                 </ResponsiveContainer>
-                </div>
               </div>
             )}
           </>
